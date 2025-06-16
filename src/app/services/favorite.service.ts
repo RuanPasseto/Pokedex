aeaedf9938580371980cf8f749f1c57ee0e77833
Ruan Carlos Passeto <ruanpasseto@gmail.com>
2025-06-16 17:55:05 -0300

    create services logic

--- src/app/services/favorite.service.ts
+++ src/app/services/favorite.service.ts
@@ -4,6 +4,36 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FavoriteService {
+  private readonly FAVORITES_KEY = 'pokemonFavorites';
 
   constructor() { }
+
+  private getFavorites(): number[] {
+    const favorites = localStorage.getItem(this.FAVORITES_KEY);
+    return favorites ? JSON.parse(favorites) : [];
+  }
+
+  isFavorite(pokemonId: number): boolean {
+    const favorites = this.getFavorites();
+    return favorites.includes(pokemonId);
+  }
+
+  toggleFavorite(pokemonId: number): boolean {
+    const favorites = this.getFavorites();
+    const index = favorites.indexOf(pokemonId);
+
+    if (index === -1) {
+      favorites.push(pokemonId);
+      localStorage.setItem(this.FAVORITES_KEY, JSON.stringify(favorites));
+      return true;
+    } else {
+      favorites.splice(index, 1);
+      localStorage.setItem(this.FAVORITES_KEY, JSON.stringify(favorites));
+      return false;
+    }
+  }
+
+  getFavoriteIds(): number[] {
+    return this.getFavorites();
+  }
 }
